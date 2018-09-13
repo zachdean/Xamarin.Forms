bf16f62870c6d631360784e3b115a4654e2280d4
Rui Marinho <me@ruimarinho.net>
2018-09-13 16:13:29 +0100

    [UnitTests]Add missing file

---
+++ Xamarin.Forms.Xaml.UnitTests/MockDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.Forms.Core.UnitTests
+{
+	public class MockDispatcher : IDispatcher
+	{
+		public void BeginInvokeOnMainThread(Action action)
+		{
+			Device.BeginInvokeOnMainThread(action);
+		}
+
+		public bool IsInvokeRequired()
+		{
+			return Device.IsInvokeRequired;
+		}
+	}
+}
