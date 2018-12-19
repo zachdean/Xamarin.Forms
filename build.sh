dbe03eafe0e64a421d1aa943f2575fe2fc93db8e
Shane Neuville <shneuvil@microsoft.com>
2018-12-19 12:16:26 -0700

    update build script

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ msbuild /t:restore Xamarin.forms.sln
 # Build XF build tasks
 msbuild Xamarin.Forms.Build.Tasks/Xamarin.Forms.Build.Tasks.csproj
 
+msbuild Xamarin.Forms.ControlGallery.Android/Xamarin.Forms.ControlGallery.Android.csproj /t:rebuild
 
 # open in vsmac and should be able to build/run from here
 open Xamarin.Forms.sln
