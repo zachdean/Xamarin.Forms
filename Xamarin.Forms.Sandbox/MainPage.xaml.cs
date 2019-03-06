9d1069a564058593b7dac11a9ddac4a505a04349
ShaneN <shane94@hotmail.com>
2019-03-06 13:18:50 -0700

    sandbox visual tests

--- Xamarin.Forms.Sandbox/MainPage.xaml.cs
+++ Xamarin.Forms.Sandbox/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using Xamarin.Forms.Xaml;
 namespace Xamarin.Forms.Sandbox
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[DesignTimeVisible(true)]
 	public partial class MainPage : ContentPage
 	{
 		public MainPage()
