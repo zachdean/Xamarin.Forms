e188ffef7e3a1f0939756fa985666d87229da867
James Clancey <james.clancey@gmail.com>
2019-04-19 15:57:58 -0800

    UWP now uses simple font names

--- Xamarin.Forms.Platform.UAP/FontExtensions.cs
+++ Xamarin.Forms.Platform.UAP/FontExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Windows.UI.Text;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
@@ -12,15 +14,16 @@ namespace Xamarin.Forms.Platform.UWP
 		public static void ApplyFont(this Control self, Font font)
 		{
 			self.FontSize = font.UseNamedSize ? font.NamedSize.GetFontSize() : font.FontSize;
-			self.FontFamily = !string.IsNullOrEmpty(font.FontFamily) ? new FontFamily(font.FontFamily) : (FontFamily)WApplication.Current.Resources["ContentControlThemeFontFamily"];
+			self.FontFamily = font.ToFontFamily();
 			self.FontStyle = font.FontAttributes.HasFlag(FontAttributes.Italic) ? FontStyle.Italic : FontStyle.Normal;
 			self.FontWeight = font.FontAttributes.HasFlag(FontAttributes.Bold) ? FontWeights.Bold : FontWeights.Normal;
 		}
 
 		public static void ApplyFont(this TextBlock self, Font font)
 		{
+			var fontFamily = new FontFamily(font.FontFamily);
 			self.FontSize = font.UseNamedSize ? font.NamedSize.GetFontSize() : font.FontSize;
-			self.FontFamily = !string.IsNullOrEmpty(font.FontFamily) ? new FontFamily(font.FontFamily) : (FontFamily)WApplication.Current.Resources["ContentControlThemeFontFamily"];
+			self.FontFamily = font.ToFontFamily();
 			self.FontStyle = font.FontAttributes.HasFlag(FontAttributes.Italic) ? FontStyle.Italic : FontStyle.Normal;
 			self.FontWeight = font.FontAttributes.HasFlag(FontAttributes.Bold) ? FontWeights.Bold : FontWeights.Normal;
 		}
@@ -28,7 +31,7 @@ namespace Xamarin.Forms.Platform.UWP
 		public static void ApplyFont(this Windows.UI.Xaml.Documents.TextElement self, Font font)
 		{
 			self.FontSize = font.UseNamedSize ? font.NamedSize.GetFontSize() : font.FontSize;
-			self.FontFamily = !string.IsNullOrEmpty(font.FontFamily) ? new FontFamily(font.FontFamily) : (FontFamily)WApplication.Current.Resources["ContentControlThemeFontFamily"];
+			self.FontFamily = font.ToFontFamily();
 			self.FontStyle = font.FontAttributes.HasFlag(FontAttributes.Italic) ? FontStyle.Italic : FontStyle.Normal;
 			self.FontWeight = font.FontAttributes.HasFlag(FontAttributes.Bold) ? FontWeights.Bold : FontWeights.Normal;
 		}
@@ -36,7 +39,7 @@ namespace Xamarin.Forms.Platform.UWP
 		internal static void ApplyFont(this Control self, IFontElement element)
 		{
 			self.FontSize = element.FontSize;
-			self.FontFamily = !string.IsNullOrEmpty(element.FontFamily) ? new FontFamily(element.FontFamily) : (FontFamily)WApplication.Current.Resources["ContentControlThemeFontFamily"];
+			self.FontFamily = element.FontFamily.ToFontFamily();
 			self.FontStyle = element.FontAttributes.HasFlag(FontAttributes.Italic) ? FontStyle.Italic : FontStyle.Normal;
 			self.FontWeight = element.FontAttributes.HasFlag(FontAttributes.Bold) ? FontWeights.Bold : FontWeights.Normal;
 		}
@@ -65,5 +68,75 @@ namespace Xamarin.Forms.Platform.UWP
 		{
 			return self.FontFamily == null && self.FontSize == Device.GetNamedSize(NamedSize.Default, typeof(Label), true) && self.FontAttributes == FontAttributes.None;
 		}
+
+		public static FontFamily ToFontFamily(this Font font) => font.FontFamily.ToFontFamily();
+		public static FontFamily ToFontFamily(this string fontFamily)
+		{
+			if (string.IsNullOrWhiteSpace(fontFamily))
+				return (FontFamily)WApplication.Current.Resources["ContentControlThemeFontFamily"];
+			//Try First if it is a system font;
+			var formated = string.Join(", ", GetAllFontPossibilities(fontFamily));
+			return new FontFamily(formated);
+		}
+
+		static IEnumerable<string> GetAllFontPossibilities(string fontFamily)
+		{
+			if(fontFamily.Contains("#"))
+			{
+				yield return fontFamily;
+			}
+			else
+			{
+				const string path = "Assets/Fonts/";
+				string[] extensions = new[]
+				{
+					".ttf",
+					".otf",
+				};
+
+				foreach (var ext in extensions)
+				{
+					var name = string.Join(' ', GetFontName(fontFamily));
+					var formated = $"{path}{fontFamily}{ext}#{name}";
+					yield return formated;
+				}
+			}
+		}
+		static IEnumerable<string> GetFontName(string fontFamily)
+		{
+			string currentString = "";
+			char lastCharacter = ' ';
+			var index = fontFamily.LastIndexOf("-");
+			var cleansedstring = index > 0 ? fontFamily.Substring(0, index) : fontFamily;
+			foreach (var c in cleansedstring)
+			{
+				if (c == '_' || c == '-')
+				{
+					yield return currentString;
+					currentString = "";
+				}
+				else
+				{
+					if (char.IsUpper(c) && char.IsLower(lastCharacter))
+					{
+						yield return currentString;
+						currentString = "";
+					}
+					currentString += c;
+					lastCharacter = c;
+				}
+			}
+			if (!string.IsNullOrWhiteSpace(currentString))
+				yield return currentString.Trim();
+
+		}
+
+		static (bool success, FontFamily fontFamily) LoadFontFamilyFromAssets(string fontFamily)
+		{
+			var font = new FontFamily(fontFamily);
+			var success = fontFamily.Contains(font.Source);
+			return (success, font);
+		}
+
 	}
 }
