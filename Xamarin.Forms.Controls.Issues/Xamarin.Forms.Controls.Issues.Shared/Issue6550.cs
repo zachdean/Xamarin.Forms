fdce08c726d889c316ca1d26458f14b25b4ce7c6
Rui Marinho <me@ruimarinho.net>
2019-06-17 20:12:36 +0100

    [Controls] Add repo for issue #6550

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue6550.cs
@@ -0,0 +1,41 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.ManualReview)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 6550, "Unable to activate instance of type Xamarin.Forms.Platform.Android.SearchBarRenderer from native handle", PlatformAffected.Default)]
+	public class Issue6550 : TestContentPage // or TestMasterDetailPage, etc ...
+	{
+		protected override void Init()
+		{
+			NavigationPage.SetTitleView(this, new SearchBar());
+			// Initialize ui here instead of ctor
+			Content = new Label
+			{
+				AutomationId = "IssuePageLabel",
+				Text = "See if I'm here"
+			};
+		}
+
+#if UITEST
+		[Test]
+		public void Issue1Test() 
+		{
+			// Delete this and all other UITEST sections if there is no way to automate the test. Otherwise, be sure to rename the test and update the Category attribute on the class. Note that you can add multiple categories.
+			RunningApp.Screenshot ("I am at Issue 1");
+			RunningApp.WaitForElement (q => q.Marked ("IssuePageLabel"));
+			RunningApp.Screenshot ("I see the Label");
+		}
+#endif
+	}
+}
