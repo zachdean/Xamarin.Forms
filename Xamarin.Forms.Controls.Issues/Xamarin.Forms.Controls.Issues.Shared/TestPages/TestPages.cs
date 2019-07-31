54c6229b499fcc4d354e61d96de196f911dcccbe
ShaneN <shane94@hotmail.com>
2019-07-31 10:23:53 -0600

    [Tests]Update to Xamarin.UITest 3.0 and NUnit 3.0 (#6332)

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/TestPages/TestPages.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/TestPages/TestPages.cs
@@ -781,7 +781,7 @@ namespace Xamarin.Forms.Controls.Issues
 	[SetUpFixture]
 	public class IssuesSetup
 	{
-		[SetUp]
+		[OneTimeSetUp]
 		public void RunBeforeAnyTests()
 		{
 			AppSetup.RunningApp = AppSetup.Setup(null);
