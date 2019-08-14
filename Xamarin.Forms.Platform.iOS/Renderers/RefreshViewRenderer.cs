79c7ed23778cc3a9c4e14f34a9fe88492468cbbb
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-08-14 15:12:11 +0200

    Code refactoring in RefreshViewRenderer (iOS)

--- Xamarin.Forms.Platform.iOS/Renderers/RefreshViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/RefreshViewRenderer.cs
@@ -12,31 +12,64 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 using System;
 using System.ComponentModel;
 using Foundation;
 using UIKit;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.iOS;
-
 
 namespace Xamarin.Forms.Platform.iOS
 {
-
-    /// <summary>
-    /// Pull to refresh layout renderer.
-    /// </summary>
-    [Preserve(AllMembers = true)]
+	[Preserve(AllMembers = true)]
     public class RefreshViewRenderer : ViewRenderer<RefreshView, UIView>
-    {
+	{
+		bool _isRefreshing;
+		bool _set;
+		nfloat _origininalY;
+		BindableProperty _rendererProperty; 
+        UIView _refreshControlParent;
+		UIRefreshControl _refreshControl;
 
-        UIRefreshControl refreshControl;
-        UIView refreshControlParent;
+		public RefreshView RefreshView
+		{
+			get { return Element; }
+		}
 
-		/// <summary>
-		/// Raises the element changed event.
-		/// </summary>
-		/// <param name="e">E.</param>
+		public bool IsRefreshing
+		{
+			get { return _isRefreshing; }
+			set
+			{
+				bool changed = IsRefreshing != value;
+
+				_isRefreshing = value;
+
+				if (_isRefreshing)
+					_refreshControl.BeginRefreshing();
+				else
+					_refreshControl.EndRefreshing();
+
+				if (changed)
+					TryOffsetRefresh(this, IsRefreshing);
+			}
+		}
+
+		public BindableProperty RendererProperty
+		{
+			get
+			{
+				if (_rendererProperty != null)
+					return _rendererProperty;
+
+				var type = Type.GetType("Xamarin.Forms.Platform.iOS.Platform, Xamarin.Forms.Platform.iOS");
+				var prop = type.GetField("RendererProperty");
+				var val = prop.GetValue(null);
+				_rendererProperty = val as BindableProperty;
+
+				return _rendererProperty;
+			}
+		}
+
 		protected override void OnElementChanged(ElementChangedEventArgs<RefreshView> e)
         {
             base.OnElementChanged(e);
@@ -44,86 +77,119 @@ namespace Xamarin.Forms.Platform.iOS
             if (e.OldElement != null || Element == null)
                 return;
 
-            refreshControl = new UIRefreshControl();
+            _refreshControl = new UIRefreshControl();
 
-            refreshControl.ValueChanged += OnRefresh;
+            _refreshControl.ValueChanged += OnRefresh;
 
-            this.refreshControlParent = this;
+            _refreshControlParent = this;
 
             UpdateColors();
             UpdateIsRefreshing();
             UpdateIsSwipeToRefreshEnabled();
         }
 
-        bool set;
-        nfloat origininalY;
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
 
-        bool TryOffsetRefresh(UIView view, bool refreshing, int index = 0)
+			if (e.PropertyName == VisualElement.IsEnabledProperty.PropertyName)
+				UpdateIsSwipeToRefreshEnabled();
+			else if (e.PropertyName == RefreshView.IsRefreshingProperty.PropertyName)
+				UpdateIsRefreshing();
+			else if (e.IsOneOf(RefreshView.RefreshColorProperty, VisualElement.BackgroundColorProperty))
+				UpdateColors();
+		}
+
+		protected override void SetBackgroundColor(Color color)
+		{
+			if (_refreshControl == null)
+				return;
+
+			if (color != Color.Default)
+				_refreshControl.BackgroundColor = color.ToUIColor();
+			else
+				_refreshControl.BackgroundColor = null;
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+
+			if (disposing)
+			{
+				if (_refreshControl != null)
+				{
+					_refreshControl.ValueChanged -= OnRefresh;
+					_refreshControl = null;
+				}
+
+				_refreshControlParent = null;
+			}
+		}
+
+		bool TryOffsetRefresh(UIView view, bool refreshing)
         {
             if (view is UITableView)
             {
                 var uiTableView = view as UITableView;
-                if (!set)
+                if (!_set)
                 {
-                    origininalY = uiTableView.ContentOffset.Y;
-                    set = true;
+                    _origininalY = uiTableView.ContentOffset.Y;
+                    _set = true;
                 }
 
-                if (origininalY < 0)
+                if (_origininalY < 0)
                     return true;
 
                 if (refreshing)
-                    uiTableView.SetContentOffset(new CoreGraphics.CGPoint(0, origininalY - refreshControl.Frame.Size.Height), true);
+                    uiTableView.SetContentOffset(new CoreGraphics.CGPoint(0, _origininalY - _refreshControl.Frame.Size.Height), true);
                 else
-                    uiTableView.SetContentOffset(new CoreGraphics.CGPoint(0, origininalY), true);
+                    uiTableView.SetContentOffset(new CoreGraphics.CGPoint(0, _origininalY), true);
                 return true;
             }
 
             if (view is UICollectionView)
             {
-
                 var uiCollectionView = view as UICollectionView;
-                if (!set)
+                if (!_set)
                 {
-                    origininalY = uiCollectionView.ContentOffset.Y;
-                    set = true;
+                    _origininalY = uiCollectionView.ContentOffset.Y;
+                    _set = true;
                 }
 
-                if (origininalY < 0)
+                if (_origininalY < 0)
                     return true;
 
                 if (refreshing)
-                    uiCollectionView.SetContentOffset(new CoreGraphics.CGPoint(0, origininalY - refreshControl.Frame.Size.Height), true);
+                    uiCollectionView.SetContentOffset(new CoreGraphics.CGPoint(0, _origininalY - _refreshControl.Frame.Size.Height), true);
                 else
-                    uiCollectionView.SetContentOffset(new CoreGraphics.CGPoint(0, origininalY), true);
+                    uiCollectionView.SetContentOffset(new CoreGraphics.CGPoint(0, _origininalY), true);
                 return true;
             }
-
 
             if (view is UIWebView)
             {
-                //can't do anything
+                // Can't do anything
                 return true;
             }
-
 
             if (view is UIScrollView)
             {
                 var uiScrollView = view as UIScrollView;
 
-                if (!set)
+                if (!_set)
                 {
-                    origininalY = uiScrollView.ContentOffset.Y;
-                    set = true;
+                    _origininalY = uiScrollView.ContentOffset.Y;
+                    _set = true;
                 }
 
-                if (origininalY < 0)
+                if (_origininalY < 0)
                     return true;
 
                 if (refreshing)
-                    uiScrollView.SetContentOffset(new CoreGraphics.CGPoint(0, origininalY - refreshControl.Frame.Size.Height), true);
+                    uiScrollView.SetContentOffset(new CoreGraphics.CGPoint(0, _origininalY - _refreshControl.Frame.Size.Height), true);
                 else
-                    uiScrollView.SetContentOffset(new CoreGraphics.CGPoint(0, origininalY), true);
+                    uiScrollView.SetContentOffset(new CoreGraphics.CGPoint(0, _origininalY), true);
                 return true;
             }
 
@@ -133,7 +199,7 @@ namespace Xamarin.Forms.Platform.iOS
             for (int i = 0; i < view.Subviews.Length; i++)
             {
                 var control = view.Subviews[i];
-                if (TryOffsetRefresh(control, refreshing, i))
+                if (TryOffsetRefresh(control, refreshing))
                     return true;
             }
 
@@ -143,43 +209,30 @@ namespace Xamarin.Forms.Platform.iOS
 
         bool TryInsertRefresh(UIView view, int index = 0)
         {
-            this.refreshControlParent = view;
-
-            if (view is UITableView)
+            _refreshControlParent = view;
+            
+            if (view is UITableView || view is UICollectionView)
             {
-                var uiTableView = view as UITableView;
-                uiTableView = view as UITableView;
-                view.InsertSubview(refreshControl, index);
-
+                view.InsertSubview(_refreshControl, index);
                 return true;
             }
-
-
-            if (view is UICollectionView)
-            {
-                var uiCollectionView = view as UICollectionView;
-                uiCollectionView = view as UICollectionView;
-                view.InsertSubview(refreshControl, index);
-                return true;
-            }
-
 
             if (view is UIWebView)
             {
                 var uiWebView = view as UIWebView;
-                uiWebView.ScrollView.InsertSubview(refreshControl, index);
+                uiWebView.ScrollView.InsertSubview(_refreshControl, index);
                 return true;
             }
 
             if (view is UIScrollView)
             {
                 var uiScrollView = view as UIScrollView;
-                view.InsertSubview(refreshControl, index);
+                view.InsertSubview(_refreshControl, index);
                 uiScrollView.AlwaysBounceVertical = true;
                 return true;
             }
-
-            if (view.Subviews == null)
+            
+			if (view.Subviews == null)
                 return false;
 
             for (int i = 0; i < view.Subviews.Length; i++)
@@ -192,53 +245,17 @@ namespace Xamarin.Forms.Platform.iOS
             return false;
         }
 
-
-
-        BindableProperty rendererProperty;
-
-        /// <summary>
-        /// Gets the bindable property.
-        /// </summary>
-        /// <returns>The bindable property.</returns>
-        BindableProperty RendererProperty
-        {
-            get
-            {
-                if (rendererProperty != null)
-                    return rendererProperty;
-
-                var type = Type.GetType("Xamarin.Forms.Platform.iOS.Platform, Xamarin.Forms.Platform.iOS");
-                var prop = type.GetField("RendererProperty");
-                var val = prop.GetValue(null);
-                rendererProperty = val as BindableProperty;
-
-                return rendererProperty;
-            }
-        }
-
         void UpdateColors()
         {
-            if (RefreshView == null || refreshControl == null)
+            if (RefreshView == null || _refreshControl == null)
                 return;
 
             if (RefreshView.RefreshColor != Color.Default)
-                refreshControl.TintColor = RefreshView.RefreshColor.ToUIColor();
+                _refreshControl.TintColor = RefreshView.RefreshColor.ToUIColor();
 
 			SetBackgroundColor(RefreshView.BackgroundColor);
         }
-
-		protected override void SetBackgroundColor(Color color)
-		{
-			if (refreshControl == null)
-				return;
-
-			if (color != Color.Default)
-				refreshControl.BackgroundColor = color.ToUIColor();
-			else
-				refreshControl.BackgroundColor = null;
-		}
-
-
+       
 		void UpdateIsRefreshing()
         {
             IsRefreshing = RefreshView.IsRefreshing;
@@ -248,56 +265,17 @@ namespace Xamarin.Forms.Platform.iOS
         {
             if (RefreshView.IsEnabled)
             {
-                this.TryInsertRefresh(this.refreshControlParent);
+                TryInsertRefresh(_refreshControlParent);
             }
             else
             {
-                if (this.refreshControl.Superview != null)
+                if (_refreshControl.Superview != null)
                 {
-                    this.refreshControl.RemoveFromSuperview();
+                    _refreshControl.RemoveFromSuperview();
                 }
             }
         }
-
-        /// <summary>
-        /// Helpers to cast our element easily
-        /// Will throw an exception if the Element is not correct
-        /// </summary>
-        /// <value>The refresh view.</value>
-        public RefreshView RefreshView
-        {
-            get { return Element; }
-        }
-
-
-
-        bool isRefreshing;
-
-        /// <summary>
-        /// Gets or sets a value indicating whether this instance is refreshing.
-        /// </summary>
-        /// <value><c>true</c> if this instance is refreshing; otherwise, <c>false</c>.</value>
-        public bool IsRefreshing
-        {
-            get { return isRefreshing; }
-            set
-            {
-                bool changed = IsRefreshing != value;
-
-                isRefreshing = value;
-                if (isRefreshing)
-                    refreshControl.BeginRefreshing();
-                else
-                    refreshControl.EndRefreshing();
-
-                if (changed)
-                    TryOffsetRefresh(this, IsRefreshing);
-            }
-        }
-
-        /// <summary>
-        /// The refresh view has been refreshed
-        /// </summary>
+                       
         void OnRefresh(object sender, EventArgs e)
         {
             if (RefreshView?.Command?.CanExecute(RefreshView?.CommandParameter) ?? false)
@@ -305,40 +283,5 @@ namespace Xamarin.Forms.Platform.iOS
                 RefreshView.Command.Execute(RefreshView?.CommandParameter);
             }
         }
-
-        /// <summary>
-        /// Raises the element property changed event.
-        /// </summary>
-        /// <param name="sender">Sender.</param>
-        /// <param name="e">E.</param>
-        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            base.OnElementPropertyChanged(sender, e);
-            if (e.PropertyName == RefreshView.IsEnabledProperty.PropertyName)
-                UpdateIsSwipeToRefreshEnabled();
-            else if (e.PropertyName == RefreshView.IsRefreshingProperty.PropertyName)
-                UpdateIsRefreshing();
-            else if (e.IsOneOf(RefreshView.RefreshColorProperty, RefreshView.BackgroundColorProperty))
-                UpdateColors();
-        }
-
-        /// <summary>
-        /// Dispose the specified disposing.
-        /// </summary>
-        /// <param name="disposing">If set to <c>true</c> disposing.</param>
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-            if (refreshControl != null)
-            {
-                refreshControl.ValueChanged -= OnRefresh;
-            }
-
-            this.refreshControlParent = null;
-        }
-
     }
-
-
 }
-
