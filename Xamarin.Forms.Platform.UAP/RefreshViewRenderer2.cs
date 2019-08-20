929e77a679a9be0c34e1339f498297eacb0c6b7b
Javier Suárez <javiersuarezruiz@hotmail.com>
2019-08-20 17:58:18 +0200

    Added another UWP RefreshView renderer using WinUI NuGet controls (RefreshContainer)

--- Xamarin.Forms.Platform.UAP/RefreshViewRenderer2.cs
+++ Xamarin.Forms.Platform.UAP/RefreshViewRenderer2.cs
@@ -1,15 +1,11 @@
 ﻿using System.ComponentModel;
-using Microsoft.UI.Xaml.Controls;
-using Windows.Foundation;
 using Windows.UI.Xaml.Media;
 
 namespace Xamarin.Forms.Platform.UWP
 {
-	public class RefreshViewRenderer : ViewRenderer<RefreshView, RefreshContainer>
+	public class RefreshViewRenderer2 : ViewRenderer<RefreshView, RefreshViewControl>
 	{
-		private Deferral _refreshCompletionDeferral;
-
-		public RefreshViewRenderer()
+		public RefreshViewRenderer2()
 		{
 			AutoPackage = false;
 		}
@@ -18,14 +14,8 @@ namespace Xamarin.Forms.Platform.UWP
 		{
 			if (Control != null && disposing)
 			{
-				Control.RefreshRequested -= OnRefresh;
-
-				if (_refreshCompletionDeferral != null)
-				{
-					_refreshCompletionDeferral.Complete();
-					_refreshCompletionDeferral.Dispose();
-					_refreshCompletionDeferral = null;
-				}
+				Control.Refresh -= OnRefresh;
+				Control.Dispose();
 			}
 
 			base.Dispose(disposing);
@@ -37,12 +27,8 @@ namespace Xamarin.Forms.Platform.UWP
 			{
 				if (Control == null)
 				{
-					var refreshControl = new RefreshContainer
-					{
-						PullDirection = RefreshPullDirection.TopToBottom
-					};
-
-					refreshControl.RefreshRequested += OnRefresh;
+					var refreshControl = new RefreshViewControl();
+					refreshControl.Refresh += OnRefresh;
 					SetNativeControl(refreshControl);
 				}
 
@@ -75,9 +61,9 @@ namespace Xamarin.Forms.Platform.UWP
 				return;
 
 			if (Element.BackgroundColor != Color.Default)
-				Control.Background = Element.BackgroundColor.ToBrush();
+				Control.RefreshBackground = Element.BackgroundColor.ToBrush();
 			else
-				Control.Background = Color.White.ToBrush();
+				Control.RefreshBackground = Color.White.ToBrush();
 		}
 
 		private void UpdateContent()
@@ -91,40 +77,25 @@ namespace Xamarin.Forms.Platform.UWP
 
 		private void UpdateIsEnabled()
 		{
-			Control.IsEnabled = Element.IsEnabled;
+			Control.IsRefreshingEnabled = Element.IsEnabled;
 		}
 
 		private void UpdateIsRefreshing()
 		{
-			if (!Element.IsRefreshing)
-			{
-				CompleteRefresh();
-			}
+			Control.IsRefreshing = Element.IsRefreshing;
 		}
 
 		private void UpdateColors()
 		{
-			Control.Foreground = Element.RefreshColor != Color.Default
+			Control.RefreshForeground = Element.RefreshColor != Color.Default
 				? Element.RefreshColor.ToBrush()
 				: (Brush)Windows.UI.Xaml.Application.Current.Resources["DefaultTextForegroundThemeBrush"];
 
 			UpdateBackgroundColor();
 		}
 
-		private void CompleteRefresh()
+		private void OnRefresh(object sender, System.EventArgs e)
 		{
-			if (_refreshCompletionDeferral != null)
-			{
-				_refreshCompletionDeferral.Complete();
-				_refreshCompletionDeferral.Dispose();
-				_refreshCompletionDeferral = null;
-			}
-		}
-
-		private void OnRefresh(object sender, RefreshRequestedEventArgs args)
-		{
-			_refreshCompletionDeferral = args.GetDeferral();
-
 			if (Element?.Command?.CanExecute(Element?.CommandParameter) ?? false)
 			{
 				Element.Command.Execute(Element?.CommandParameter);
