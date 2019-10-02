6b3d5c037477b5eb151395bd9793135b3c6dd7c8
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-10-02 18:00:04 +0200

    Added Issue5386 Core Gallery sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue5386.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.ManualReview)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 5386, "ToolbarItem IsEnabled doesnt read initial binding", PlatformAffected.Android)]
+	public partial class Issue5386 : TestContentPage
+	{
+		public Issue5386()
+		{
+			InitializeComponent();
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class Issue5386ViewModel : BindableObject
+	{
+		public bool Enabled { get; set; }
+		public string EnabledText { get; set; }
+		public Command ChangeToggleCommand { get; set; }
+		public Command ToolbarTappedCommand { get; set; }
+		public Command ChangeToggleBackgroundThreadCommand { get; set; }
+
+		public Issue5386ViewModel()
+		{
+			ChangeToggleCommand = new Command(ChangeToggle);
+			ChangeToggleBackgroundThreadCommand = new Command(ChangeToggleBackgroundThread);
+			ToolbarTappedCommand = new Command(ToolbarTapped);
+			EnabledText = Enabled ? "Enabled" : "Disabled";
+		}
+
+		private async void ChangeToggleBackgroundThread(object obj)
+		{
+			await Task.Run(() =>
+			{
+				ChangeToggle();
+			});
+		}
+
+		private async void ToolbarTapped(object obj)
+		{
+			await Application.Current.MainPage.DisplayAlert("ToolbarItem", "You tapped me and I obeyed", "OK");
+		}
+
+		private void ChangeToggle()
+		{
+			Enabled = !Enabled;
+			OnPropertyChanged("Enabled");
+			EnabledText = Enabled ? "Enabled" : "Disabled";
+			OnPropertyChanged("EnabledText");
+		}
+	}
+}
