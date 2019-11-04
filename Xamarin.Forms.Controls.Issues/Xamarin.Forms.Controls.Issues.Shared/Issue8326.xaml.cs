89b6bf3db286233abfe0179fa02cd522a2a49df7
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-11-04 11:41:10 +0100

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8326.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+
+#if UITEST
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.CollectionView)]
+#endif
+#if APP
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 8326, "[Bug] CollectionView: empty view doesn't show if collection view contains a header", PlatformAffected.Android)]
+	public partial class Issue8326 : TestContentPage
+	{
+#if APP
+		public Issue8326()
+		{
+			InitializeComponent();            
+			collectionView.ItemsSource = new List<Issue8326Model> ();
+		}
+#endif
+
+		protected override void Init()
+		{
+			Title = "Issue 8326";
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class Issue8326Model
+	{
+		public string Text { get; set; }
+	}
+}
