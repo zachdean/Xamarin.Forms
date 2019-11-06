626b7141a3a532ef4901f8de19bb2169f60798d2
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-11-06 10:44:18 +0100

    Updated repro sample to be able to show/hide the EmptyView

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8326.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue8326.xaml.cs
@@ -2,6 +2,8 @@
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 #if UITEST
 using Xamarin.UITest;
@@ -25,8 +27,8 @@ namespace Xamarin.Forms.Controls.Issues
 #if APP
 		public Issue8326()
 		{
-			InitializeComponent();            
-			collectionView.ItemsSource = new List<Issue8326Model> ();
+			InitializeComponent();
+			BindingContext = new Issue8326ViewModel();
 		}
 #endif
 
@@ -40,5 +42,42 @@ namespace Xamarin.Forms.Controls.Issues
 	public class Issue8326Model
 	{
 		public string Text { get; set; }
+	}
+
+	[Preserve(AllMembers = true)]
+	public class Issue8326ViewModel : BindableObject
+	{
+		ObservableCollection<Issue8326Model> _items;
+
+		public Issue8326ViewModel()
+		{
+			Items = new ObservableCollection<Issue8326Model>();
+		}
+
+		public ObservableCollection<Issue8326Model> Items
+		{
+			get { return _items; }
+			set
+			{
+				_items = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public ICommand ClearItemsCommand => new Command(ClearItems);
+		public ICommand AddItemsCommand => new Command(AddItems);
+
+		void ClearItems()
+		{
+			Items.Clear();
+		}
+
+		void AddItems()
+		{
+			for (int i = 0; i < 20; i++)
+			{
+				Items.Add(new Issue8326Model { Text = $"Item {i+1}" });
+			}
+		}
 	}
 }
