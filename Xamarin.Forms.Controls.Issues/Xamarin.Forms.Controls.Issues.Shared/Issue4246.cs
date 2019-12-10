274f3578825eefaeff1640df1bf30bfd51dbccaa
Gerald Versluis <gerald@verslu.is>
2019-12-10 14:00:40 +0100

    Added repro

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue4246.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 4246, "XF 3.3 sets TabbedPage color based on NavigationPage.BarBackgroundColorProperty", PlatformAffected.Android | PlatformAffected.iOS)]
+	public class Issue4246 : TestTabbedPage
+	{
+		protected override void Init()
+		{
+			Children.Add(new NavigationPage(new ContentPage())
+			{
+				Title = "1"
+			});
+
+			Children.Add(new NavigationPage(new ContentPage())
+			{
+				Title = "2"
+			});
+
+			Children.Add(new NavigationPage(new ContentPage())
+			{
+				Title = "3"
+			});
+
+			Children.Add(new NavigationPage(new ContentPage())
+			{
+				Title = "4"
+			});
+
+			SetValue(NavigationPage.BarTextColorProperty, Color.Black);
+			SetValue(NavigationPage.BarBackgroundColorProperty, Color.Pink);
+		}
+	}
+}
