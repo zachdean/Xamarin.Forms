153419b379d3cb20c49c84c08b437676f79407dd
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-03-23 11:12:37 +0100

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue10049.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using System.Collections.Generic;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.UITest.iOS;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 10049, "[Bug] AdaptiveTrigger crashes when paired with a Setter with TargetName", PlatformAffected.UWP)]
+	public partial class Issue10049 : TestContentPage
+    {
+        public Issue10049()
+		{
+#if APP
+			Device.SetFlags(new List<string> { ExperimentalFlags.StateTriggersExperimental });
+			Title = "Issue 10049";
+            InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+		
+		}
+	}
+}
