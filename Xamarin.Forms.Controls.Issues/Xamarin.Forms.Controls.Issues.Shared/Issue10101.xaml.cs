012c640b6af6d4b3df8d8dcb1dc04a699500d7f8
Gerald Versluis <gerald@verslu.is>
2020-06-02 16:47:38 +0200

    Added repro from pr

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue10101.xaml.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue10101.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
 using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 
@@ -8,6 +10,36 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue(IssueTracker.Github, 10101, "Changing text on a Label on macOS resets font color to Black", PlatformAffected.macOS)]
 	public partial class Issue10101 : TestContentPage
 	{
+		class ColorItem
+		{
+			public Color Color { get; set; } = Color.Red;
+			public string Name { get; set; }
+		}
+
+		readonly Random _random = new Random();
+
+		static List<ColorItem> GetSystemColorList() =>
+			new List<ColorItem>
+			{
+				new ColorItem() {Color = Color.Accent, Name = "Accent"},
+				new ColorItem() {Color = Color.AliceBlue, Name = "AliceBlue"},
+				new ColorItem() {Color = Color.MintCream, Name = "MintCream"},
+				new ColorItem() {Color = Color.MistyRose, Name = "MistyRose"},
+				new ColorItem() {Color = Color.Moccasin, Name = "Moccasin"},
+				new ColorItem() {Color = Color.NavajoWhite, Name = "NavajoWhite"},
+				new ColorItem() {Color = Color.Navy, Name = "Navy"},
+				new ColorItem() {Color = Color.OldLace, Name = "OldLace"},
+				new ColorItem() {Color = Color.MidnightBlue, Name = "MidnightBlue"},
+				new ColorItem() {Color = Color.Olive, Name = "Olive"},
+				new ColorItem() {Color = Color.Orange, Name = "Orange"},
+				new ColorItem() {Color = Color.OrangeRed, Name = "OrangeRed"},
+				new ColorItem() {Color = Color.Orchid, Name = "Orchid"},
+				new ColorItem() {Color = Color.PaleGoldenrod, Name = "PaleGoldenrod"},
+				new ColorItem() {Color = Color.PaleGreen, Name = "PaleGreen"},
+				new ColorItem() {Color = Color.PaleTurquoise, Name = "PaleTurquoise"},
+				new ColorItem() {Color = Color.OliveDrab, Name = "OliveDrab"}
+			};
+
 		protected override void Init()
 		{
 
@@ -17,6 +49,21 @@ namespace Xamarin.Forms.Controls.Issues
 		public Issue10101()
 		{
 			InitializeComponent();
+			OnTapped(null, null);
+		}
+
+		ColorItem RandomColor()
+		{
+			var colors = GetSystemColorList();
+			return colors[_random.Next(0, colors.Count - 1)];
+		}
+
+		void OnTapped(object sender, EventArgs e)
+		{
+			var color = RandomColor();
+			labelColor.TextColor = color.Color;
+			labelText.Text = $"TextColor should be {color.Name}";
+			labelColor.Text = $"TextColor should be {color.Name}";
 		}
 
 		void button_Clicked(System.Object sender, System.EventArgs e)
