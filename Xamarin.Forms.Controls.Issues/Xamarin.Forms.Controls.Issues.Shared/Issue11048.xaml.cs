6023f2a0c2de361afde2f1814f312cd10d9429c1
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-06-18 14:17:25 +0200

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11048.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 11048, "[Bug][Android] Shapes: flower not drawing correctly on Android", PlatformAffected.Android)]
+	public partial class Issue11048 : ContentPage
+	{
+		public Issue11048()
+		{
+#if APP
+			InitializeComponent();
+#endif
+
+		}
+	}
+
+}
