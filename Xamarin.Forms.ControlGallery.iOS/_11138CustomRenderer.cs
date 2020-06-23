27fa48c0a774f07201a3cb06399320eb63297857
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-06-23 16:55:20 +0200

    Added repro sample

---
+++ Xamarin.Forms.ControlGallery.iOS/_11138CustomRenderer.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.ControlGallery.iOS;
+using Xamarin.Forms.Controls.Issues;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Issue11138NavigationPage), typeof(_11138CustomRenderer))]
+namespace Xamarin.Forms.ControlGallery.iOS
+{
+	public class _11138CustomRenderer : NavigationRenderer
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            UINavigationBar.Appearance.SetBackgroundImage(new UIImage(), UIBarMetrics.Default);
+            UINavigationBar.Appearance.ShadowImage = new UIImage();
+            UINavigationBar.Appearance.BackgroundColor = UIColor.Clear;
+            UINavigationBar.Appearance.Translucent = true;
+        }
+    }
+}
---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11138.cs
@@ -0,0 +1,63 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.Navigation)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 11138,
+		"[Bug] iOS Translucent not longer works with UIAppearance",
+		PlatformAffected.iOS)]
+	public class Issue11138 : TestContentPage
+	{
+		public Issue11138()
+		{
+		}
+
+		protected override void Init()
+		{
+			Title = "Issue 11138";
+
+			var layout = new StackLayout
+			{
+				BackgroundColor = Color.LightGray
+			};
+
+			var navigateButton = new Button
+			{
+				Text = "Navigate"
+			};
+
+			layout.Children.Add(navigateButton);
+
+			Content = layout;
+
+			navigateButton.Clicked += (sender, args) =>
+			{
+				Navigation.PushAsync(new Issue11138NavigationPage(new Issue11138()));
+			};
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class Issue11138NavigationPage : NavigationPage
+	{
+		public Issue11138NavigationPage()
+		{
+
+		}
+
+		public Issue11138NavigationPage(Page root) : base(root)
+		{
+
+		}
+	}
+}
