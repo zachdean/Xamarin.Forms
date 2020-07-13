6ad21d30f4e0bd5214fc27542683425c8ee9beeb
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-07-13 13:49:50 +0200

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11405.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using System;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Github, 11405,
+		"[UWP] Cannot unselect SelectedItem for CollectionView",
+		PlatformAffected.UWP)]
+	public partial class Issue11405 : TestContentPage
+	{
+		public Issue11405()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+		}
+#if APP
+		void OnUnselectClicked(object sender, EventArgs e)
+		{
+			MonkeyCollectionView.SelectedItem = null;
+		}
+#endif
+	}
+}
