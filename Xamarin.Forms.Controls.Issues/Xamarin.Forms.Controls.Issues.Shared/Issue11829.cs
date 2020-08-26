18869bf132b8594b5ebb668695ba467aaaf96c0e
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-08-26 14:22:43 +0200

    Updated the sample to allow to modify the LineHeight dynamically

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11829.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11829.cs
@@ -41,13 +41,74 @@ namespace Xamarin.Forms.Controls.Issues
 			{
 				TextDecorations = TextDecorations.Underline,
 				LineHeight = 2,
-				Text = "Underline using LineHeight"
+				Text = "Underline using LineHeight",
+				Margin = new Thickness(0, 12)
 			};
+
+			var textDecorationsLayout = new StackLayout
+			{
+				Orientation = StackOrientation.Horizontal
+			};
+
+			var textDecorationsCheckBox = new CheckBox
+			{
+				IsChecked = true,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			var textDecorationsText = new Label
+			{
+				Text = "Underline",
+				WidthRequest = 100,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			textDecorationsLayout.Children.Add(textDecorationsText);
+			textDecorationsLayout.Children.Add(textDecorationsCheckBox);
+
+			var lineHeightLayout = new StackLayout
+			{
+				Orientation = StackOrientation.Horizontal
+			};
+
+			var lineHeightSlider = new Slider
+			{
+				Maximum = 4,
+				Minimum = 2,
+				Value = 2,
+				WidthRequest = 150,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			var lineHeightText = new Label
+			{
+				Text = "LineHeight",
+				WidthRequest = 100,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			lineHeightLayout.Children.Add(lineHeightText);
+			lineHeightLayout.Children.Add(lineHeightSlider);
 
 			layout.Children.Add(instructions);
 			layout.Children.Add(label);
+			layout.Children.Add(textDecorationsLayout);
+			layout.Children.Add(lineHeightLayout);
 
 			Content = layout;
+
+			textDecorationsCheckBox.CheckedChanged += (sender, args) =>
+			{
+				if (args.Value)
+					label.TextDecorations = TextDecorations.Underline;
+				else
+					label.TextDecorations = TextDecorations.None;
+			};
+
+			lineHeightSlider.ValueChanged += (sender, args) =>
+			{
+				label.LineHeight = args.NewValue;
+			};
 		}
 	}
 }
