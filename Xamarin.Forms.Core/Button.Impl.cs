0fd2bd92ea7a5c67e4adf504e4e71b08331264ec
Shane Neuville <shneuvil@microsoft.com>
2020-09-10 12:02:25 -0500

    - add reference

---
+++ Xamarin.Forms.Core/Button.Impl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Platform;
+
+namespace Xamarin.Forms
+{
+	public partial class Button : IButton
+	{		
+		public IViewRenderer Renderer { get; set; }
+
+		TextType IText.TextType => throw new NotImplementedException();
+
+		Color IText.Color => throw new NotImplementedException();
+
+		bool IFrameworkElement.IsEnabled => throw new NotImplementedException();
+
+		Color IFrameworkElement.BackgroundColor => throw new NotImplementedException();
+
+		Rectangle IFrameworkElement.Frame => throw new NotImplementedException();
+
+		IFrameworkElement IFrameworkElement.Parent => throw new NotImplementedException();
+
+		SizeRequest IFrameworkElement.DesiredSize => throw new NotImplementedException();
+
+		bool IFrameworkElement.IsMeasureValid => throw new NotImplementedException();
+
+		bool IFrameworkElement.IsArrangeValid => throw new NotImplementedException();
+
+		void IFrameworkElement.Arrange(Rectangle bounds)
+		{
+			throw new NotImplementedException();
+		}
+
+		void IButton.Clicked()
+		{
+			throw new NotImplementedException();
+		}
+
+		void IFrameworkElement.InvalidateArrange()
+		{
+			throw new NotImplementedException();
+		}
+
+		void IFrameworkElement.InvalidateMeasure()
+		{
+			throw new NotImplementedException();
+		}
+
+		SizeRequest IFrameworkElement.Measure(double widthConstraint, double heightConstraint)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- src/Platform.Handlers/samples/Sample/PocoUI/Button.cs
+++ src/Platform.Handlers/samples/Sample/PocoUI/Button.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 using Xamarin.Platform;
 
 namespace Sample.PocoUI
