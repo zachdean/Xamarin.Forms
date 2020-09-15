57a789797f3461ef5423bb657e7ff00b4fe693d9
Shane Neuville <shneuvil@microsoft.com>
2020-09-15 14:34:35 -0500

    - uibutton

--- src/Platform.Handlers/src/Xamarin.Platform.Handlers/Handlers/Button/ButtonHandler.iOS.cs
+++ src/Platform.Handlers/src/Xamarin.Platform.Handlers/Handlers/Button/ButtonHandler.iOS.cs
@@ -5,7 +5,7 @@ namespace Xamarin.Platform.Handlers
 {
 	public partial class ButtonHandler : AbstractViewHandler<IButton, UIButton>
 	{
-		protected override UIButton CreateView() => throw new NotImplementedException();
+		protected override UIButton CreateView() => new UIButton();
 
 		public static void MapText(IViewHandler handler, IButton view) { }
 	}
