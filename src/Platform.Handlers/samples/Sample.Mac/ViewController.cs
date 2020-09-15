72fcd39b3e236d8d705056247122b50b2b6bae86
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-09-15 18:36:59 +0200

    Added macOS sample

---
+++ src/Platform.Handlers/samples/Sample.Mac/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Platform;
+
+namespace Sample.Mac
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController(IntPtr handle) : base(handle)
+		{
+			var app = new MyApp();
+			View = (app.MainPage as ContentPage).Content.ToNative();
+		}
+
+		public override NSObject RepresentedObject
+		{
+			get
+			{
+				return base.RepresentedObject;
+			}
+			set
+			{
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+	}
+}
