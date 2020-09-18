9d25272a46b68746e63b4dd9fde9c0d1df17404c
rachelkang <rachelkang@microsoft.com>
2020-09-18 10:23:39 -0400

    [Controls] Set CreateDefaultMainPage again

--- Xamarin.Forms.Controls/App.cs
+++ Xamarin.Forms.Controls/App.cs
@@ -33,8 +33,7 @@ namespace Xamarin.Forms.Controls
 		{
 			_testCloudService = DependencyService.Get<ITestCloudService>();
 
-			//SetMainPage(CreateDefaultMainPage());
-			SetMainPage(new Issue11703());
+			SetMainPage(CreateDefaultMainPage());
 
 			//TestMainPageSwitches();
 
