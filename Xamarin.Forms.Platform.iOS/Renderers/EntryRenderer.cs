d68a9b60fdf49db604babcece2c55418cb8b4db5
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-10-07 10:39:34 +0200

    Tint the clear button image in iOS

--- Xamarin.Forms.Platform.iOS/Renderers/EntryRenderer.cs
+++ Xamarin.Forms.Platform.iOS/Renderers/EntryRenderer.cs
@@ -48,6 +48,8 @@ namespace Xamarin.Forms.Platform.iOS
 		static readonly int baseHeight = 30;
 		static CGSize initialSize = CGSize.Empty;
 
+		UIImage _defaultClearImage;
+
 		public EntryRendererBase()
 		{
 		}
@@ -551,7 +553,63 @@ namespace Xamarin.Forms.Platform.iOS
 
 		void UpdateClearButtonVisibility()
 		{
-			Control.ClearButtonMode = Element.ClearButtonVisibility == ClearButtonVisibility.WhileEditing ? UITextFieldViewMode.WhileEditing : UITextFieldViewMode.Never;
+			if (Element.ClearButtonVisibility == ClearButtonVisibility.WhileEditing)
+			{
+				Control.ClearButtonMode = UITextFieldViewMode.WhileEditing;
+				UpdateClearButtonColor();
+			}
+			else
+				Control.ClearButtonMode = UITextFieldViewMode.Never;
+		}
+
+		void UpdateClearButtonColor()
+		{
+			if (Control.ValueForKey(new NSString("clearButton")) is UIButton clearButton)
+			{
+				clearButton.TintColor = Element.TextColor.ToUIColor();
+
+				if(_defaultClearImage == null)
+					_defaultClearImage = clearButton.ImageForState(UIControlState.Highlighted);
+
+				if(Element.TextColor == Color.Default)
+				{
+					clearButton.SetImage(_defaultClearImage, UIControlState.Normal);
+					clearButton.SetImage(_defaultClearImage, UIControlState.Highlighted);
+				}
+				else
+				{
+					var tintedClearImage = GetClearButtonTintImage(_defaultClearImage, Element.TextColor.ToUIColor());
+
+					clearButton.SetImage(tintedClearImage, UIControlState.Normal);
+					clearButton.SetImage(tintedClearImage, UIControlState.Highlighted);
+				}
+			}
+		}
+
+		UIImage GetClearButtonTintImage(UIImage image, UIColor color)
+		{
+			var size = image.Size;
+
+			UIGraphics.BeginImageContextWithOptions(size, false, UIScreen.MainScreen.Scale);
+
+			if (UIGraphics.GetCurrentContext() == null)
+				return null;
+
+			var context = UIGraphics.GetCurrentContext();
+
+			image.Draw(CGPoint.Empty, CGBlendMode.Normal, 1.0f);
+			context?.SetFillColor(color.CGColor);
+			context?.SetBlendMode(CGBlendMode.SourceIn);
+			context?.SetAlpha(1.0f);
+
+			var rect = new CGRect(CGPoint.Empty.X, CGPoint.Empty.Y, image.Size.Width, image.Size.Height);
+			context?.FillRect(rect);
+
+			var tintedImage = UIGraphics.GetImageFromCurrentImageContext();
+
+			UIGraphics.EndImageContext();
+
+			return tintedImage;
 		}
 	}
 }
