e700d74865109d6abd950893485315627b18d8dc
rachelkang <rachelkang@microsoft.com>
2020-10-09 14:30:04 -0400

    Replace manual UI test with automated unit test

--- Xamarin.Forms.Platform.Android.UnitTests/ButtonTests.cs
+++ Xamarin.Forms.Platform.Android.UnitTests/ButtonTests.cs
@@ -31,5 +31,23 @@ namespace Xamarin.Forms.Platform.Android.UnitTests
 			int compareTo = Color.White.ToAndroid();
 			Assert.AreNotEqual(compareTo, disabledColor);
 		}
+
+		[Test, Category("Button")]
+		[Description("Account for user's setting of styles property textAllCaps")]
+		[Issue(IssueTracker.Github, 11703, "[Bug] Android textAllCaps no longer works")]
+		public async Task Issue11703Test()
+		{
+			var button = new Button { Text = "foo" };
+			var actual = await GetControlProperty(button, control => control.TransformationMethod);
+
+			button.TextTransform = TextTransform.Uppercase;
+			var newValue = await GetControlProperty(button, control => control.TransformationMethod);
+
+			button.TextTransform = TextTransform.Default;
+			var newerValue = await GetControlProperty(button, control => control.TransformationMethod);
+
+			Assert.AreNotEqual(actual, newValue);
+			Assert.AreEqual(actual.GetType(), newerValue.GetType());
+		}
 	}
 }
