317ea37d09870d4f2f03dd40b6943a28289c3040
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-10-22 14:09:15 +0200

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12454.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.Shape)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 12454,
+		"[Bug] Why Rectangle couldn't have the same Width as Label",
+		PlatformAffected.Android)]
+	public partial class Issue12454 : TestContentPage
+	{
+		public Issue12454()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
