a8821054878fc1dfbf7b93d2b1a5b985569b0f81
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-10-28 09:13:04 +0100

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12609.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms.CustomAttributes;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Github, 12609, "Shapes.Path render erro", PlatformAffected.All)]
+	public partial class Issue12609 : TestContentPage
+	{
+		public Issue12609()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+
+}
