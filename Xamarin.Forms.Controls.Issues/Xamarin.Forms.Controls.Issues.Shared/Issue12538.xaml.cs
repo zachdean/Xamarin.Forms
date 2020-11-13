caf9ef25f2759bf7ebe2a1678f24c6d36b38351b
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-11-13 13:07:19 +0100

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12538.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.CollectionView)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 12538, "[Bug] Shapes do not work with StaticResource Fill Colour", PlatformAffected.UWP)]
+	public partial class Issue12538 : TestContentPage
+	{
+#if APP
+		public Issue12538()
+		{
+			InitializeComponent();
+		}
+#endif
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
