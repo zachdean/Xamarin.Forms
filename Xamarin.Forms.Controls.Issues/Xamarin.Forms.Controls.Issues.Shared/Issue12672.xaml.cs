fd315d0fed9a83aaff28f4c6ae980953acb9b45c
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-11-17 14:53:09 +0100

    Fixed iOS Path rendering issue

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12672.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 12672, "[iOS] Path is not displayed correctly",
+		PlatformAffected.iOS)]
+	public partial class Issue12672 : TestContentPage
+	{
+		public Issue12672()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
