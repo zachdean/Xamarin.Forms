474a03a7683bdbeda0f85363877f1087cda95c07
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-11-27 09:35:55 +0100

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11514.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 11514, "[Bug] iOS crashes when clipping with GeometryGroup", PlatformAffected.iOS)]
+	public partial class Issue11514 : TestContentPage
+	{
+		public Issue11514()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
