06b22ceabc323877c31657a1751fecb385274941
E.Z. Hart <hartez@gmail.com>
2020-12-02 15:58:42 -0700

    Automate test for issue 10110

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue10110.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue10110.cs
@@ -16,6 +16,9 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue(IssueTracker.Github, 10110, "CollectionView EmptyView doesn't show up on UWP HorizontalList", PlatformAffected.UWP)]
 	public class Issue10110 : TestContentPage
 	{
+		const string VerticalEmptyView = "Empty Vertical List as String";
+		const string HorizontalEmptyView = "Empty Horizontal List as String";
+
 		public Issue10110()
 		{
 			Title = "Issue 10110";
@@ -31,14 +34,14 @@ namespace Xamarin.Forms.Controls.Issues
 			var verticalCollectionView = new CollectionView
 			{
 				BackgroundColor = Color.LightBlue,
-				EmptyView = "Empty Vertical List as String"
+				EmptyView = VerticalEmptyView
 			};
 
 			var horizontalCollectionView = new CollectionView
 			{
 				BackgroundColor = Color.LightCoral,
 				ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Horizontal),
-				EmptyView = "Empty Horizontal List as String"
+				EmptyView = HorizontalEmptyView
 			};
 
 			layout.Children.Add(verticalCollectionView);
@@ -54,5 +57,14 @@ namespace Xamarin.Forms.Controls.Issues
 		{
 
 		}
+
+#if UITEST
+		[Test]
+		public void EmptyViewShowsUpInHorizontalAndVerticalList()
+		{
+			RunningApp.WaitForElement(HorizontalEmptyView);
+			RunningApp.WaitForElement(VerticalEmptyView);
+		}
+#endif
 	}
 }
