c4107dcec1c6a11d16a5b1a4e67bcb155366fb63
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-12-03 18:54:29 +0100

    Wrap the Editor Placeholder Text

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue13037.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms.CustomAttributes;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Github, 13037, "[Bug] Placeholder text in Editor control does not wrap text on UWP",	
+		PlatformAffected.UWP)]
+	public partial class Issue13037 : TestContentPage
+	{
+		public Issue13037()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
