5c0545b0d652f3966f9918c3abddf73161775e19
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-12-09 16:13:29 +0100

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue13058.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 13058, "[Bug] GradientBrush not working with Material on Android", PlatformAffected.Android)]
+	public partial class Issue13058 : ContentPage
+	{
+		public Issue13058()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+	}
+}
