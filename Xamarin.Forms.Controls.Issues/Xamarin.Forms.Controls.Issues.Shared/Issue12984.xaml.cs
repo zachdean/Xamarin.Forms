af3c29082ce76d8af42aa2c11ac4e385c93b8005
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-12-14 20:18:47 +0100

    Fix Button VisualState Disabled in UWP

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue12984.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.CollectionView)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 12984, "[Bug][UWP] VisualState Disabled not working on Button",
+		PlatformAffected.UWP)]
+	public partial class Issue12984 : TestContentPage
+	{
+		public Issue12984()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
