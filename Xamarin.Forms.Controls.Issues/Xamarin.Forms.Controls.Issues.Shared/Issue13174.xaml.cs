25305b47c7de6d70da4791d9f4b34a29ba3f778a
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-12-17 13:16:30 +0100

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue13174.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.Shape)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 13174,
+		"[Bug] Line's Stroke can't be rendered correctly on iOS while setting to a color with alpha",
+		PlatformAffected.iOS)]
+	public partial class Issue13174 : TestContentPage
+	{
+		public Issue13174()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
