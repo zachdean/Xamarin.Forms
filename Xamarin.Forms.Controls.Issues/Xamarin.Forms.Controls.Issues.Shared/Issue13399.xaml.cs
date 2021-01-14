2bece8d3eeb3a6663071770cb6513a660f93b7ba
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-01-14 14:07:50 +0100

    Added repro sample

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue13399.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+#if UITEST
+using Xamarin.UITest;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 13399,
+		"[Bug] The specified child already has a parent",
+		PlatformAffected.Android)]
+	public partial class Issue13399 : TestContentPage
+	{
+		public Issue13399()
+		{
+#if APP
+			InitializeComponent();
+			BindingContext = new Issue13399ViewModel();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class Issue13399Model
+	{
+		public int Id { get; set; }
+		public string Text { get; set; }
+	}
+
+	[Preserve(AllMembers = true)]
+	public class Issue13399ViewModel : BindableObject
+	{
+		ObservableCollection<Issue13399Model> _items;
+
+		public Issue13399ViewModel()
+		{
+			Items = new ObservableCollection<Issue13399Model>();
+			LoadItems();
+		}
+
+		public ObservableCollection<Issue13399Model> Items
+		{
+			get { return _items; }
+			set
+			{
+				_items = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public ICommand DeleteItemCommand => new Command(ExecuteDeleteItem);
+
+		void LoadItems()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Items.Add(new Issue13399Model { Id = i + 1, Text = $"Item {i + 1}" });
+			}
+		}
+
+		void ExecuteDeleteItem()
+		{
+			var index = Items.Count - 1;
+			Items.RemoveAt(index);
+		}
+	}
+}
