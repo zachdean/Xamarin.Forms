c5b54a009af169b0f1c6ae0b2a0f0c1a99af1b65
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-01-20 12:49:02 +0100

    Changes registering handlers

--- src/Platform.Handlers/samples/Sample/Platform.cs
+++ src/Platform.Handlers/samples/Sample/Platform.cs
@@ -14,12 +14,12 @@ namespace Sample
 				return;
 
 			HasInit = true;
-      
-      RegistrarHandlers.Handlers.Register<Button, ButtonHandler>();
-      RegistrarHandlers.Handlers.Register<DatePicker, DatePickerHandler>();
-      RegistrarHandlers.Handlers.Register<Label, LabelHandler>();
+
+			RegistrarHandlers.Handlers.Register<Button, ButtonHandler>();
+			RegistrarHandlers.Handlers.Register<DatePicker, DatePickerHandler>();
+			RegistrarHandlers.Handlers.Register<Label, LabelHandler>();
 			RegistrarHandlers.Handlers.Register<Slider, SliderHandler>();
-      
+
 			RegistrarHandlers.Handlers.Register<VerticalStackLayout, LayoutHandler>();
 			RegistrarHandlers.Handlers.Register<HorizontalStackLayout, LayoutHandler>();
 			RegistrarHandlers.Handlers.Register<FlexLayout, LayoutHandler>();
