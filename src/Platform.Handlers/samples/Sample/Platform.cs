f70fa3f6c0f8537e95ffdff908c1e6f379f89c0b
Shane Neuville <shneuvil@microsoft.com>
2021-02-01 13:17:48 -0600

    - fix up platform heads

--- src/Platform.Handlers/samples/Sample/Platform.cs
+++ src/Platform.Handlers/samples/Sample/Platform.cs
@@ -33,7 +33,7 @@ namespace Sample
 
 		void RegisterLegacyRendererAgainstFormsControl()
 		{
-#if MONOANDROID
+#if MONOANDROID && !NET6
 
 			// register renderer with old registrar so it can get shimmed
 			// This will move to some extension method
